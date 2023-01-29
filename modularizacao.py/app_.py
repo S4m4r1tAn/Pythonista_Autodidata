ad2f94c46385b913778a6037e9edea9bdc4e190a
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-01-29 16:11:10 +0000

    modularizacao

--- modularizacao.py/app_.py
+++ modularizacao.py/app_.py
@@ -1,22 +1,7 @@
-""" class Camera():
-    def __init__(self, resolucao_maxima):
-        resolucao_maxima = resolucao_maxima
-        
-    def tirar_foto(self):
-        print('Tirando foto.')
-    
-    def aumentar_zoom(self):
-        print('Aumentando ZOOM.')
-        
-    def diminuir_zoom(self):
-        print('Diminuir ZOOM.') """
-        
-""" def iniciar_banco_de_dados():
-    print('Iniciando conexao.')
-    print('Criando tabelas iniciais.')
-    
-def buscar_usuarios():
-    print('Buscando usuarios.') """
+from banco_de_dados import *
+from configuracoes import *
+from processamento_imagens import *
 
-""" senhas = 123456
-usuarios_maximos = 5 """
+#
+#
+#
