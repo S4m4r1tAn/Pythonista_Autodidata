f9166760b2e7885656d423d3a4e838118791e0bf
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-03 22:45:33 +0000

    Desafio_API

---
+++ variaveis/API/Desafio_API.py
@@ -0,0 +1,24 @@
+""" DESAFIO API músicas 🥇
+### 1. Defnir o objetivo da API:
+Iremos montar uma api de músicas, onde deverá ser possível, 
+consultar todas canções disponíveis, consultar uma canção individual, 
+editar canções existentes e também excluir músicas existentes.
+### 2. Qual será o URL base da API?
+Iremos utilizar o url base 'localhost'
+### 3. Quais são os endpoints?
+Disponibilize endpoints para consultar, editar, criar e excluir
+### 4. Quais recursos serão disponibilizados pela API?
+Informações sobre canções
+### 5. Quais verbos http serão disponibilizados?
+* GET
+* POST
+* PUT
+* DELETE
+### 6. Quais são os URLs completos para cada um?
+* GET http://localhost:5000/cancoes
+* GET http://localhost:5000/cancoes/1
+* POST http://localhost:5000/cancoes
+* PUT http://localhost:5000/cancoes/1
+* DELETE http://localhost:5000/cancoes/1
+### 7. Qual deve ser a estrutura de cada canção
+ - lista de dicionários, que contem cancao e estilo """
