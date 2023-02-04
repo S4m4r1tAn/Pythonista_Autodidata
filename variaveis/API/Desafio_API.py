ae1bedf864a337207e7cfc7c7ce08828ecdf7bcc
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-04 17:02:32 +0000

    desafio_API_resolucao

--- variaveis/API/Desafio_API.py
+++ variaveis/API/Desafio_API.py
@@ -1,77 +1,48 @@
 from flask import Flask, jsonify, request
 from pprint import pprint
 
-### DESAFIO API músicas 🥇
-### 1. Definir o objetivo da API:
-""" Iremos montar uma api de músicas, onde deverá ser possível, 
-consultar todas canções disponíveis, consultar uma canção individual, 
-editar canções existentes e também excluir músicas existentes. """
 app = Flask(__name__)
-musicas = [
-     {
-     "cancao": "Amor I Love you!",
-     "estilo": "MPB"
-     },
-     {
-     "cancao": "A gente se entrega.",
-     "estilo": "SERTANEJO"
+cancoes = [
+    {
+        "cancao": "Amor I Love you!",
+        "estilo": "MPB"
     },
     {
-     "cancao": "Diario de um detento.",
-     "estilo": "RAP"
+        "cancao": "A gente se entrega.",
+        "estilo": "SERTANEJO"
+    },
+    {
+        "cancao": "Diario de um detento.",
+        "estilo": "RAP"
     }
 ]
-### 2. Qual será o URL base da API?
-### Iremos utilizar o url base 'localhost' """ #rota padra: http://localhost:5000
-app.route('/')
-def obter_musicas():
-    return jsonify(musicas)
 
-### 3. Quais são os endpoints?
-### Disponibilize endpoints para consultar, editar, criar e excluir
-# Get com Id - GET http://localhost:5000/musicas/1
-@app.route('/musicas/<int:indice>', methods=['GET'])
-def obter_musicas_por_indice(indice):
-    return jsonify(musicas[indice])
+app.route('/cancoes', methods=['GET']) #rota padrao: http://localhost:5000
+def obter_todas_cancoes():
+    return jsonify(cancoes)
 
-# Criar uma nova postagem - POST http://localhost:5000/musicas
-@app.route('/musicas', methods=['POST'])
-def nova_musica():
-    musica = request.get_json()
-    musicas.append(musica)
-    return jsonify(musica, 200)
+@app.route('/cancoes/<int:cancao_id>', methods=['GET']) # Get com Id: http://localhost:5000/cancoes/1
+def obter_cancao_por_indice(cancao_id):
+    return jsonify(cancoes[cancao_id])
 
-# Alterar uma postagem existente - PUT http://localhost:5000/musicas/1
-@app.route('/musica/<int:indice>', methods=['PUT'])
-def alterar_musica(indice):
-    musica_alterada = request.get_json()
-    musicas[indice].update(musica_alterada)
-    return jsonify(musicas[indice],200)
+@app.route('/cancoes', methods=['POST']) # Criar uma nova postagem: http://localhost:5000/cancoes
+def nova_cancao():
+    cancao = request.get_json()
+    cancoes.append(cancao)
+    return jsonify(f'A cancao: {cancao} foi adicionada com sucesso', 200)
 
-# Excluir uma postagem - DELETE - http://localhost:5000/musicas/1
-@app.route('/musica/<int:indice>', methods=['DELETE'])
-def excluir_musica(indice):
+@app.route('/cancoes/<int:cancao_id>', methods=['PUT']) # Alterar uma postagem existente: http://localhost:5000/cancoes/1
+def alterar_cancao(cancao_id):
+    cancao_alterada = request.get_json()
+    cancoes[cancao_id].update(cancao_alterada)
+    return jsonify(cancoes[cancao_id], 200)
+
+@app.route('/cancoes/<int:cancao_id>', methods=['DELETE']) # Excluir uma postagem: http://localhost:5000/cancoes/1
+def excluir_cancao(cancao_id):
     try:
-        if musicas[indice] is not None:
-            del musicas[indice]
-            return jsonify(f'Foi excluida a musica {musicas[indice]}', 200)
+        del cancoes[cancao_id]
+        return jsonify(f'Foi excluida a musica {cancao_id} com sucesso!')
     except:
-        return jsonify('Nao foi possivel encontrar a musica para exclusao', 404)
+        jsonify('Nao foi encontrada uma cancao com este id', 404)
     
-### 4. Quais recursos serão disponibilizados pela API?
-### Informações sobre canções
-### 5. Quais verbos http serão disponibilizados?
-""" * GET
-* POST
-* PUT
-* DELETE """
-### 6. Quais são os URLs completos para cada um?
-""" * GET http://localhost:5000/cancoes
-* GET http://localhost:5000/cancoes/1
-* POST http://localhost:5000/cancoes
-* PUT http://localhost:5000/cancoes/1
-* DELETE http://localhost:5000/cancoes/1 """
-### 7. Qual deve ser a estrutura de cada canção
-### - lista de dicionários, que contem cancao e estilo
-
 app.run(port=5000, host='localhost', debug=True)
