0ee780505d1abca2c7b457715028bed9489e080d
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-07 13:26:07 +0000

    Banco_de_Dados_SQLAlchemy

--- CRUD_Banco_de_Dados/app_sql.py
+++ CRUD_Banco_de_Dados/app_sql.py
@@ -59,19 +59,59 @@ def obter_autores():
 
 @app.route('/autores/<int:id_autor>', methods=['GET'])
 def obter_autor_por_id(id_autor):
-    pass
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+    if not autor:
+        return jsonify(f'Autor nao encontrado!')
+    autor_atual = {}
+    autor_atual['id_autor'] = autor.id_autor
+    autor_atual['nome'] = autor.nome
+    autor_atual['email'] = autor.email
+    
+    return jsonify({"autor": autor_atual})
 
 @app.route('/autores', methods=['POST'])
 def novo_autor():
-    pass
+    novo_autor = request.get_json()
+    autor = Autor(
+        nome=novo_autor['nome'], senha=novo_autor['senha'], email=novo_autor['email'])
+    
+    db.session.add(autor)
+    db.session.commit()
 
 @app.route('/autores/<int:id_autor>', methods=['PUT'])
 def alterar_autor(id_autor):
-    pass
+    usuario_a_alterar = request.get_json()
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+    if not autor:
+        return jsonify({'Mensagem': 'Este usuario nao foi encontrado!'})
+    
+    try:
+        if usuario_a_alterar['nome']:
+            autor.nome = usuario_a_alterar['nome']
+    except:
+        pass
+    try:
+        if usuario_a_alterar['email']:
+            autor.nome = usuario_a_alterar['email']
+    except:
+        pass
+    try:
+        if usuario_a_alterar['senha']:
+            autor.nome = usuario_a_alterar['senha']
+    except:
+        pass
+
+    db.session.commit()
+    return jsonify({'Mensagem': 'Usuario alterado com sucesso.'})
 
 @app.route('/autores/<int:id_autor>', methods=['DELETE'])
 def excluir_autor(id_autor):
-    pass
-
+    autor_existente = Autor.query.filter_by(id_autor=id_autor).first()
+    if not autor_existente:
+        return({'Mensagem': 'Este autor nao foi encontrado.'})
+    db.session.delete(autor_existente)
+    db.session.commit()
+    
+    return jsonify({'Mensagem': 'Autor excluido com sucesso!'})
 
 app.run(port=5000,host='localhost',debug=True)
