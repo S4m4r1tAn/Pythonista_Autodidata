deb2401e566e52c1df9c48f0cfe1109cd82fe4f0
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-11 21:57:49 +0000

    commit()

--- List_Compreheension/desafio2.py
+++ List_Compreheension/desafio2.py
@@ -1,9 +1,5 @@
 cores = ['vermelho', 'azul', 'verde', 'amarelo', 'rosa', 'preto']
 ordem_cor = [1, 2, 3, 4, 5, 6]
-print([i for i in cores])
 
-#print('**RESULTADO 2**')
-def lista_de_cores(ordem_cor):
-    return ordem_cor + cores
-
-print([lista_de_cores(ordem_cor) for cor in cores])
+lista_de_cores = [(i for i in [1, 2, 3, 4, 5, 6] for i in ['vermelho', 'azul', 'verde', 'amarelo', 'rosa', 'preto'])]
+print(lista_de_cores)
