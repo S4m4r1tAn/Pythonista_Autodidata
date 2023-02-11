2a475342f2177b76e68ec2a99423d7d66738b0e3
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-11 22:18:34 +0000

    commit (desafios 1, 2 e 3 feitos)

--- List_Compreheension/desafio2.py
+++ List_Compreheension/desafio2.py
@@ -1,5 +1,5 @@
+from pprint import pprint
 cores = ['vermelho', 'azul', 'verde', 'amarelo', 'rosa', 'preto']
 ordem_cor = [1, 2, 3, 4, 5, 6]
 
-lista_de_cores = [(i for i in [1, 2, 3, 4, 5, 6] for i in ['vermelho', 'azul', 'verde', 'amarelo', 'rosa', 'preto'])]
-print(lista_de_cores)
+pprint([str(cores.index(i)+1) + ' - ' + i for i in cores])
--- List_Compreheension/desafio3.py
+++ List_Compreheension/desafio3.py
@@ -5,17 +5,17 @@ participantes = ['Joel', 'Jessica', 'Maria', 'Cris', 'Larissa', 'Rafael', 'Marcu
 pagamento_realizado = ['Joel', 'Jessica', 'Maria', 'Cris']
 
 print('**RESULTADO 1**')
-resultado = [colored(i + ' PAGO', 'green', attrs=['bold']) if i in pagamento_realizado else colored(i + ' NAO PAGO', 'red', attrs=['bold']) for i in participantes]
+resultado = [colored(i + ' - '' PAGO', 'green', attrs=['bold']) if i in pagamento_realizado else colored(i + ' - '' DEVENDO', 'red', attrs=['bold']) for i in participantes]
 for r in resultado:
     print(r)
 print('**' *18)
 
 print('**RESULTADO 2**')   
-pprint([i + ' PAGO' if i in pagamento_realizado else i + ' NAO PAGO' for i in participantes])
+pprint([i + ' - '' PAGO' if i in pagamento_realizado else i + ' - '' DEVENDO' for i in participantes])
 print('**' *18)
 
 print('**RESULTADO 3**')
 def pagadores(i):
-    return i + ' PAGO'
+    return i + ' - '' PAGO'
 pprint([pagadores(i) if i in pagamento_realizado else i for i in participantes])
 print('**' *18)
