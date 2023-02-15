6609a7ec389773b7a2aba4df5e2dd6edf2eba622
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-15 15:18:33 +0000

    commit atualizado

--- Intro a Threads e Threading/desafios_thread/desafio2.py
+++ Intro a Threads e Threading/desafios_thread/desafio2.py
@@ -2,16 +2,18 @@ import threading
 from time import sleep
     
 # THREADS PARA DADOS EM MASSA.
-def extrair_precos(site, produto):
+def extrair_precos(site, produto, preco):
     print(f'Entrando no \033[33;1m{site}\033[m e pesquisando sobre \033[30;41;1;4m{produto}\033[m.')
     sleep(5)
-    print(f'O ITEM: \033[30;46;1;4m{produto}\033[m FOI PROCESSADO COM SUCESSO!')
+    print(f'O PRECO DO ITEM: \033[30;46;1;4m{produto}\033[m FOI PROCESSADO COM SUCESSO!')
+    print(f'O \033[30;46;1;4m{produto}\033[m CUSTA \033[32;43;1m{preco}\033[m.')
 
 threads = []
 produtos = ['CELULAR', 'MONITOR', 'FONE DE OUVIDO', 'ALTO-FALANTE', 'COMPUTADOR']
+precos = ['£1350,00', '£325,00','£89,00','£175,00', '£3890,00']
 for i in range(5):
     nova_thread = threading.Thread(
-        target=extrair_precos, args=('https://best.aliexpress.com', produtos[i]))
+        target=extrair_precos, args=('https://best.aliexpress.com', produtos[i], precos[i]))
     threads.append(nova_thread)
     
 for thread in threads:
