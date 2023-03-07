c266127b79f83b7113416fb5b1223b034a3ea4c8
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-07 17:36:53 +0000

    commit

--- json/arquivos_json.py
+++
@@ -1,6 +0,0 @@
-import json
-from pathlib import Path
-
-pikachu_json = Path('pikachu.json').read_text()
-resultado = json.loads(pikachu_json)
-print(resultado['abilities'][1]['ability']['name'].upper())
